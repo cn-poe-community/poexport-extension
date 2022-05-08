7bfd98d6aa6f71f6439d0bc1ab052211e604266c
hybo <52079867+me1ting@users.noreply.github.com>
2022-05-08 11:17:52 +0800

    Revert "Import vuestic-ui"
    This reverts commit 1c30e79700a8919d14c0e9c71fe48bda7e54f0b8.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import Index from './Index.vue'
-import { VuesticPlugin } from 'vuestic-ui'
-import 'vuestic-ui/dist/vuestic-ui.css'
 
-createApp(Index).use(VuesticPlugin).mount('#app')
+createApp(Index).mount('#app')
