e4266433be99e03606104e91051567f8447f2dff
hybo <52079867+me1ting@users.noreply.github.com>
2022-07-13 17:54:58 +0800

    Add popup menu.

---
+++ public/menu.js
@@ -0,0 +1,7 @@
+document.querySelector('#settings').addEventListener('click', function () {
+    if (chrome.runtime.openOptionsPage) {
+        chrome.runtime.openOptionsPage();
+    } else {
+        window.open(chrome.runtime.getURL('index.html'));
+    }
+});
