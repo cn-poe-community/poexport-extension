0dc669c2870bde5f1457b3c7fd72d64904a4f904
me1ting <52079867+me1ting@users.noreply.github.com>
2024-04-11 22:42:18 +0800

    feat: use fixed server ip

--- public/export_wroker.js
+++ public/export_wroker.js
@@ -1,115 +1,59 @@
-const QUERY_SERVER_URL = 'https://poe.pathof.top/export/server'
-
-async function getServer() {
-  const { exportServer } = await chrome.storage.local.get('exportServer')
-  return exportServer
-}
-
-async function setServer(server) {
-  return chrome.storage.local.set({ exportServer: server })
-}
-
-async function checkServer() {
-  console.log(`checking server status`)
-  const server = await getServer()
-  if (!server || server.length === 0) {
-    console.log(`no available server in storage`)
-    return false
-  }
-  try {
-    console.log(`checking server ${server}`)
-    const res = await fetch(`${server}/status`)
-    if (res.status !== 200) {
-      console.log(`server ${server} returns ${res.status}`)
-      return false
-    }
-    const json = await res.json()
-    if (!json.code || json.code !== 200) {
-      console.log(`server ${server} returns ${json}`)
-      return false
-    }
-  } catch (err) {
-    console.log(err)
-    return false
-  }
-  console.log(`server ${server} works well`)
-  return true
-}
-
-async function loadServer() {
-  try {
-    const res = await fetch(QUERY_SERVER_URL)
-    if (res.status !== 200) {
-      console.log(`load server failed: ${res.status}`)
-      return
-    }
-    const server = await res.text()
-    console.log(`loaded server ${server}`)
-    await setServer(server)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-async function onLoad() {
-  const ok = await checkServer()
-  if (!ok) {
-    console.log('reloading server')
-    loadServer()
-  }
-}
+const EXPORT_SERVER = 'http://120.77.179.194:8888'
+const HTTP_TIMEOUT = 3000
 
 async function transform(compressed, sendResponse) {
-  const server = await getServer()
-  if (!server || server.length === 0) {
-    sendResponse({
-      code: 1,
-      msg: '无可用Export服务器，刷新重试或联系开发者'
-    })
-    return
-  }
+  console.log(`fetching ${EXPORT_SERVER}/transform`)
+  let res = null
+
+  const controller = new AbortController()
+  const timeoutId = setTimeout(() => controller.abort(), HTTP_TIMEOUT)
+
   try {
-    console.log(`fetching ${server}/transform`)
-    const res = await fetch(`${server}/transform`, {
+    res = await fetch(`${EXPORT_SERVER}/transform`, {
       method: 'post',
       headers: {
         'Content-type': 'octet-stream'
       },
-      body: compressed
-    })
-    if (res.status !== 200) {
-      sendResponse({
-        code: 1,
-        msg: `导出错误: ${res.status}`
-      })
-      return
-    }
-    const apiResp = await res.json()
-    const code = apiResp.code
-    if (code !== 200) {
-      sendResponse({
-        code: 1,
-        msg: `导出错误: ${apiResp.msg}`
-      })
-      return
-    }
-
-    sendResponse({
-      code: 0,
-      data: apiResp.data.xml
+      body: compressed,
+      signal: controller.signal
     })
   } catch (err) {
     console.log(err)
     sendResponse({
       code: 1,
-      msg: `${err}`
+      msg: `无法访问Export服务器，刷新重试或联系开发者`
     })
+    return
+  }finally{
+    clearTimeout(timeoutId)
   }
+
+  if (res.status !== 200) {
+    sendResponse({
+      code: 1,
+      msg: `导出错误: ${res.status}`
+    })
+    return
+  }
+  const apiResp = await res.json()
+  const code = apiResp.code
+  if (code !== 200) {
+    sendResponse({
+      code: 1,
+      msg: `导出错误: ${apiResp.msg}`
+    })
+    return
+  }
+
+  sendResponse({
+    code: 0,
+    data: apiResp.data.xml
+  })
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.task === 'load') {
-    onLoad()
+    //加载逻辑已移除，因此目前不做任何事情
   } else if (request.task === 'transform') {
     const data = new Uint8Array(request.compressed)
     transform(data, sendResponse)
